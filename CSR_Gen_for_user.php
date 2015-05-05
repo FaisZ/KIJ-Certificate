54274581e6a356dace67cee2a03855c7303c8ef5
ZolaMajendra <gianfzola@gmail.com>
2015-05-06 02:42:15 +0700

    Perbaikan CSR_Generate
    Perbaikan CSR_Generate

--- CSR_Gen_for_user.php
+++ CSR_Gen_for_user.php
@@ -7,45 +7,44 @@
 
 	<?php
 	
-	
-	$dataarr = array(
-//		"Nama Perusahaan" => $_POST["nama_perusahaan"];
-//		"Alamat Perusahaan" => $_POST["alamat"];
-//		"Alamat e-mail" => $_POST["email"];
-//		"Website" => $_POST["web"];
-//		"key" => $_POST["key"];
-		"Nama Perusahaan" => "XY",
-		"Alamat Perusahaan" => "XY",
-		"Alamat e-mail" => "XY",
-		"Website" => "XY",
-		"key" => "XY"
-	);
+	$dn = array(
+    "countryName" => $_POST["negara"],
+    "stateOrProvinceName" => $_POST["prov"],
+    "localityName" => $_POST["kota"],
+    "organizationName" => $_POST["org"],
+    "organizationalUnitName" => $_POST["unit"],
+    "commonName" => $_POST["nama"],
+    "emailAddress" => $_POST["email"]
+		);
 
-	$pk = "RAHASIA";
+		// Generate a new private (and public) key pair
+		$privkey = openssl_pkey_new();
 
-	//CSR Generation
-	$csr = openssl_csr_new($dataarr, $pk);
-	
-	//export csr
-	openssl_csr_export($csr, $csrout) and var_dump($csrout);
+		// Generate a certificate signing request
+		$csr = openssl_csr_new($dn, $privkey);
 
-	//download the csr generated
-	if (file_exists($csrout)) {
-		header('Content-Description: File Transfer');
-		header('Content-Type: application/octet-stream');
-		header('Content-Disposition: attachment; filename='.basename($csrout));
-		header('Expires: 0');
-		header('Cache-Control: must-revalidate');
-		header('Pragma: public');
-		header('Content-Length: ' . filesize($csrout));
-		readfile($csrout);
-		exit;
-	}
-	// Show any errors that occurred here
-	while (($e = openssl_error_string()) !== false) {
-		echo $e . "\n";
-	}
+		// You will usually want to create a self-signed certificate at this
+		// point until your CA fulfills your request.
+		// This creates a self-signed cert that is valid for 365 days
+		$sscert = openssl_csr_sign($csr, null, $privkey, 365);
 
+		// Now you will want to preserve your private key, CSR and self-signed
+		// cert so that they can be installed into your web server, mail server
+		// or mail client (depending on the intended use of the certificate).
+		// This example shows how to get those things into variables, but you
+		// can also store them directly into files.
+		// Typically, you will send the CSR on to your CA who will then issue
+		// you with the "real" certificate.
+		openssl_csr_export($csr, $csrout) and var_dump($csrout);
+		openssl_x509_export($sscert, $certout) and var_dump($certout);
+		openssl_pkey_export($privkey, $pkeyout, "mypassword") and var_dump($pkeyout);
+
+		// Show any errors that occurred here
+		while (($e = openssl_error_string()) !== false) {
+			echo $e . "\n";
+		}
+		openssl_x509_export_to_file($sscert, 'C:\xampp\htdocs\KIJ-Certificate\Certificate.crt', FALSE);
+		
 	?>
 
 	</body>
