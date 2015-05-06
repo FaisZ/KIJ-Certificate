1078645749d0f13885c7e743785148d16e577faa
FaisZ <fais9999@ymail.com>
2015-05-06 09:00:44 +0700

    CA update

--- Self_Cert_Gen_for_CA.php
+++ Self_Cert_Gen_for_CA.php
@@ -10,13 +10,16 @@
 //	$usercsr = readfile
 	
 	$CAdataarr = array(
-		"Nama Perusahaan" => "KIJ inc."
-		"Alamat Perusahaan" => "Keputih"
-		"Alamat e-mail" => "KIJ@its.ac.id"
-		"Website" => "www.KIJ.com"
+    "countryName" => "INA",
+    "stateOrProvinceName" => "Jawa Timur",
+    "localityName" => "Surabaya",
+    "organizationName" => "CA_ITS",
+    "organizationalUnitName" => "CA",
+    "commonName" => "CA",
+    "emailAddress" => "CA@its.ac.id"
 	);
 
-	$CApk = "RAHASIA";
+	$CApk = openssl_pkey_new();
 
 	//CA's CSR Generation
 	$CAcsr = openssl_csr_new($CAdataarr, $CApk);
@@ -27,9 +30,14 @@
 	//Sign user's CSR into a CERT
 	$usercert = openssl_csr_sign($Usercsr, $CAcert, $CAprivkey, 365);
 
+	//export private key
+	openssl_pkey_export($privkey, $pkeyout, "mypassword") and var_dump($pkeyout);
+
 	//export user certificate
 	openssl_x509_export($usercert, $usercertout) and var_dump($usercertout);
 
+	openssl_x509_export_to_file($usercert, 'C:\xampp\htdocs\KIJ-Certificate\UserCertificate.crt', FALSE);
+	
 	// Show any errors that occurred here
 	while (($e = openssl_error_string()) !== false) {
 		echo $e . "\n";
